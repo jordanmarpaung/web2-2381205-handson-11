e4acb0470d8d53093aec8afc8eab85591e3da0b5
Jordan Marpaung <2381025@unai.edu>
2025-04-15 16:47:53 +0700

    first commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,15 +26,15 @@ const Navbar = () => {
 								className="h-8 w-auto"
 							/>
 						</div>
-						<div className="hidden sm:ml-6 sm:block">
-							<div className="flex space-x-4">
-								{navigation.map((item) => (
-									<NavLink to={item.to} key={item.name}
-										className={({isActive}) => {
-												return classNames(
-													isActive ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-													'rounded-md px-3 py-2 text-sm font-medium',
-												)
+			<div className="hidden sm:ml-6 sm:block">
+				<div className="flex space-x-4">
+					{navigation.map((item) => (
+						<NavLink to={item.to} key={item.name}
+							className={({isActive}) => {
+									return classNames(
+										isActive ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+											'rounded-md px-3 py-2 text-sm font-medium',
+											)
 										}}>
 										{item.name}
 									</NavLink>
